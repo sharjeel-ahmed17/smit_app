d988563a2b29927e14af27d4e71e34a9eb64b480
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-10-30 11:03:26 +0500

    setup routing

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        {/* left  */}
+        <View></View>
+        {/* right */}
+        <View>
+          <Image
+            // source={uri('')}
+            source={require('./hero.png')}
+            width={200}
+            height={200}
+          />
+        </View>
+
+      </View>
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
